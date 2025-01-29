08e248e285acd358e763967d1c2a1944c4320f6c
Crissjn <cracus89@gmail.com>
2025-01-29 14:35:28 +0100

    First commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React Pizza</Link>
+      <p>
+        Criss 
+        
+      </p>
+    </header>
+  )
+}
+
+export default Header
