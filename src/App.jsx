3c903521bf8d6de09bbfac8de59d9fa5f00141ad
Crissjn <cracus89@gmail.com>
2025-01-31 12:14:26 +0100

    added action for new order

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Home from './ui/Home'
 import Menu,{loader as menuLoader} from './features/menu/Menu'
 import Cart from  './features/cart/Cart'
 import Order,{loader as orderLoader} from './features/order/Order'
-import CreateOrder from './features/order/CreateOrder'
+import CreateOrder,{action as createOrderAction} from './features/order/CreateOrder'
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error"
+
 
 const router = createBrowserRouter([
   {
@@ -29,7 +30,8 @@ const router = createBrowserRouter([
       },
       {
         path : '/order/new',
-        element :  <CreateOrder />
+        element :  <CreateOrder />,
+        action : createOrderAction,
       },
       {
         path:  '/order/:orderId',
